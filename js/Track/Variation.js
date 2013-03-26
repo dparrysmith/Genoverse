9b5ec0f808067adecc28b7bd5c98fd750c5661b5
Simon Brent <simon.brent@gmail.com>
2013-03-26 13:01:10 +0000

    Fix inserts

--- js/Track/Variation.js
+++ js/Track/Variation.js
@@ -2,22 +2,19 @@ Genoverse.Track.Variation = Genoverse.Track.extend({
   labels     : 'overlay',
   autoHeight : 'force',
   
-  // Add triangles at the bottom of inserts
+  // Add inserts with triangles
   decorateFeature: function (feature, context, scale) {
     var i = feature.decorations.length;
-    var x = feature.position[scale].X;
-    var decoration;
     
     while (i--) {
-      decoration = feature.decorations[i];
+      context.fillStyle = feature.decorations[i].color;
       
-      context.fillStyle = decoration.color;
-      
-      if (decoration.style === 'insertion') {
+      if (feature.decorations[i].style === 'insertion') {
+        context.fillRect(feature.position[scale].X, feature.position[scale].Y, 1, feature.position[scale].height);
         context.beginPath();
-        context.moveTo(x - 3, this.featureHeight + 1);
-        context.lineTo(x,     this.featureHeight - 3);
-        context.lineTo(x + 3, this.featureHeight + 1);
+        context.moveTo(feature.position[scale].X - 3, this.featureHeight + 1);
+        context.lineTo(feature.position[scale].X,     this.featureHeight - 3);
+        context.lineTo(feature.position[scale].X + 3, this.featureHeight + 1);
         context.fill();
       }
     }
