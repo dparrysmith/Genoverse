ecad221af9ca43aebf227d92741800f5ef5f9361
Simon Brent <simon.brent@gmail.com>
2013-03-28 09:24:35 +0000

    Set labels to overlay to correct text positioning in Chrome Add support for reverse strand

--- js/Track/Scaleline.js
+++ js/Track/Scaleline.js
@@ -1,7 +1,9 @@
 Genoverse.Track.Scaleline = Genoverse.Track.extend({
+  strand         : 1,
   color          : '#000000',
   height         : 12,
   featuresHeight : 14,
+  labels         : 'overlay',
   inherit        : [ 'Static' ],
   
   resize: $.noop,
@@ -24,28 +26,35 @@ Genoverse.Track.Scaleline = Genoverse.Track.extend({
     }
     
     var text   = this.formatLabel(this.browser.length);
-    var text2  = 'Forward strand';
+    var text2  = this.strand === 1 ? 'Forward strand' : 'Reverse strand';
     var width1 = this.context.measureText(text).width;
     var width2 = this.context.measureText(text2).width;
+    var bg     = this.browser.colors.background;
+    var x1, x2;
+    
+    switch (this.strand) {
+      case 1  : x1 = 0;  x2 = this.width - width2 - 40; break;
+      default : x1 = 25; x2 = 30; break;
+    }
     
     this.scaleline = [
-      { x: 0,                                       y: this.height / 2, width: (this.width - width1 - 10) / 2,            height: 1 },
-      { x: width1 + (this.width - width1 + 10) / 2, y: this.height / 2, width: ((this.width - width1) / 2) - width2 - 45, height: 1 },
-      { x: this.width - 30,                         y: this.height / 2, width: 5,                                         height: 1, decorations: true },
-      { x: (this.width - width1) / 2,               y: 2,               width: width1,                                    height: 0, color: '#FFFFFF', labelColor: this.color, label: text  },
-      { x: this.width - width2 - 35,                y: 2,               width: width2,                                    height: 0, color: '#FFFFFF', labelColor: this.color, label: text2 }
+      { x: x1,                             y: this.height / 2, width: this.width - 25, height: 1, decorations: true },
+      { x: (this.width - width1 - 10) / 2, y: 0,               width: width1 + 10,     height: this.height, color: bg, labelColor: this.color, labelWidth: width1, label: text  },
+      { x: x2,                             y: 0,               width: width2 + 10,     height: this.height, color: bg, labelColor: this.color, labelWidth: width2, label: text2 },
     ];
     
     return this.base(this.scaleline, params);
   },
   
   decorateFeature: function (feature, context, scale) {
+    var x = this.strand === 1 ? this.width - 25 : 25;
+    
     context.strokeStyle = this.color;
     
     context.beginPath();
-    context.moveTo(this.width - 25, this.height * 0.25);
-    context.lineTo(this.width - 5,  this.height * 0.5);
-    context.lineTo(this.width - 25, this.height * 0.75);
+    context.moveTo(x,                      this.height * 0.25);
+    context.lineTo(x + (this.strand * 20), this.height * 0.5);
+    context.lineTo(x,                      this.height * 0.75);
     context.closePath();
     context.stroke();
     context.fill();
