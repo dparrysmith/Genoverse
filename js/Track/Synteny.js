9666a7ca0c41c4d7f0b51c416254039a30eb052f
Simon Brent <simon.brent@gmail.com>
2013-05-09 14:36:37 +0100

    Make synteny work again

--- js/Track/Synteny.js
+++ js/Track/Synteny.js
@@ -3,45 +3,55 @@ Genoverse.Track.Synteny = Genoverse.Track.extend({
   bump          : true,
   allData       : true,
   
-  parseData: function (data, start, end) {
-    data.features = data[this.urlParams.id];
+  init: function () {
+    this.primaryTrack = this.id;
+    this.dfds         = {};
     
-    var i = data.features.length;
-    
-    if (this.url) {
-      var j = this.browser.tracks.length;
-      
-      this.colors = data.colors;
-      
-      while (j--) {
-        if (this.browser.tracks[j].type === 'Synteny' && this.browser.tracks[j] !== this) {
-          this.browser.tracks[j].colors = this.colors;
-          this.browser.tracks[j].parseData($.extend(true, {}, data));
-        }
+    for (var i = 0; i < this.browser.tracks.length; i++) {
+      if (this.browser.tracks[i].type === 'Synteny') {
+        this.primaryTrack = this.browser.tracks[i].id;
+        break;
       }
     }
     
-    while (i--) {
+    if (this.browser.tracksById[this.primaryTrack] && this.browser.tracksById[this.primaryTrack].colors) {
+      this.colors = this.browser.tracksById[this.primaryTrack].colors;
+    }
+    
+    this.base();
+  },
+  
+  getData: function (start, end) {
+    if (this.colors) {
+      return this.base(start, end);
+    }
+    
+    if (this.primaryTrack === this.id) {
+      this.urlParams.colors = 1;
+      
+      return this.base(start, end).done(function () {
+        for (var i = 0; i < this.browser.tracks.length; i++) {
+          if (this.browser.tracks[i].type === 'Synteny' && this.browser.tracks[i].id !== this.id) {
+            this.browser.tracks[i].colors = this.colors;
+            this.browser.tracks[i].getData(start, end).done(function () { this.dfds[start + ':' + end].resolveWith(this); });
+          }
+        }
+      });
+    }
+    
+    return this.dfds[start + ':' + end] = $.Deferred();
+  },
+  
+  parseData: function (data, start, end) {
+    if (data.colors) {
+      this.colors = data.colors;
+      delete this.urlParams.colors;
+    }
+    
+    for (var i = 0; i < data.features.length; i++) {
       data.features[i].color = data.features[i].labelColor = this.colors[data.features[i].colorId];
     }
     
     this.base(data, start, end);
-  },
-  
-  makeImage: function (params) {
-    if (this.url) {
-      var track       = this;
-      var otherTracks = $.map(this.browser.tracks, function (t) { if (t.type === 'Synteny' && t.id !== track.id) { t.url = false; return t.id; } });
-      
-      return this.base(params).done(function () {
-        var i = otherTracks.length;
-        
-        while (i--) {
-          this.browser.tracksById[otherTracks[i]].makeImage(params);
-        }
-      });
-    } else if (this.colors) {
-      return this.base(params); 
-    }
   }
 });
